61c5a31cce7a882cb3f45827f7d803bcfe1b1006
Vy Truong <trminhvy@gmail.com>
2024-06-02 01:47:06 -0600

    up2

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 export default function Page(){
     return(
         <main>
-            {/* p */}
+            {/*  */}
             <div className="bg-slate-800 p-4" > 
             <h1 className="text-3xl text-white font-bold mx-4 p-4">Shopping List</h1>   
             <ItemList/>
